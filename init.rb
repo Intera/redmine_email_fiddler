80b8f2b77f91dc8c69bb5af468f56ede0b727fa2
Julian Kalbhenn <jkalbhenn@intera.de>
2019-04-08 09:47:40 +0000

    fix plugin settings page 404

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 require 'mailer_patch'
 
-Redmine::Plugin.register :email_fiddler do
+Redmine::Plugin.register :redmine_email_fiddler do
   name 'Email Fiddler plugin'
   author 'Rafael Vargas'
   description 'A Redmine plugin to enable fliddling with the notification emails subjects'
@@ -8,28 +8,27 @@ Redmine::Plugin.register :email_fiddler do
   url 'https://github.com/rsvargas/redmine_email_fiddler'
   author_url 'https://github.com/rsvargas'
 
-  settings( 
+  settings(
     :default => {
       'issue_add_subject' => '',
       'issue_edit_subject' => '',
-    }, 
-    :partial =>  'settings/email_fiddler'
+    },
+    :partial =>  'settings/redmine_email_fiddler'
   )
 end
 
 if Rails::VERSION::MAJOR >= 3
-    # ActiveSupport::Reloader for rails >= 5
-    # ActionDispatch::Callbacks for rails < 5
-    reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Callbacks	
-   
-    reloader.to_prepare do
-        require_dependency 'mailer'
-        Mailer.send(:include, EmailFiddlerMailerPatch)
-    end
-else
-    Dispatcher.to_prepare do
-        require_dependency 'mailer'
-        Mailer.send(:include, EmailFiddlerMailerPatch)
-    end
-end
+  # ActiveSupport::Reloader for rails >= 5
+  # ActionDispatch::Callbacks for rails < 5
+  reloader = defined?(ActiveSupport::Reloader) ? ActiveSupport::Reloader : ActionDispatch::Callbacks
 
+  reloader.to_prepare do
+    require_dependency 'mailer'
+    Mailer.send(:include, EmailFiddlerMailerPatch)
+  end
+else
+  Dispatcher.to_prepare do
+    require_dependency 'mailer'
+    Mailer.send(:include, EmailFiddlerMailerPatch)
+  end
+end
